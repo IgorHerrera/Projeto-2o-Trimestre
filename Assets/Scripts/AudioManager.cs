826141cf0eca77caa533d4d816b4274968a8f6b4
Igor Herrera <igorherrerasantos@gmail.com>
2018-08-10 11:08:06 -0300

    vídeo 10 - colocando audio e erro no jump, finalizando os scripts de audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioSFx audioSFx;
+
+    public AudioPlayer audioPlayer;
+
+    public static AudioManager instance;
+
+    void Awake()
+    {
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+        }
+    }
+    public void PlayCoinPickupSound(GameObject obj)
+    {
+        AudioSource.PlayClipAtPoint(audioSFx.coinPickup, obj.transform.position);
+    }
+
+    public void PlayJumpSound(GameObject obj)
+    {
+        AudioSource.PlayClipAtPoint(audioPlayer.jump, obj.transform.position);
+    }
+}
